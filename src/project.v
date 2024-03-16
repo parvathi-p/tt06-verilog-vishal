3c0f518206473ae3131314803dc50d1d196e9a0f
parvathi-p <163625619+parvathi-p@users.noreply.github.com>
2024-03-16 12:47:11 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -23,6 +23,6 @@ module tt_um_halfadder (
     assign uio_in = 0;
   assign uio_out = 0;   
   assign uio_oe  = 0;
-    assign uo_out[0]=ui_in[0] ^|ui_in[1]; //assigning sum
-    assign uo_out[1]=ui_in[0] & ui_in[1]; //assigning carry
+    assign uo_out[0] = ui_in[0] ^ ui_in[1]; //assigning sum
+    assign uo_out[1] = ui_in[0] & ui_in[1]; //assigning carry
 endmodule
